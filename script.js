aa70da61b3632ce894fcf2a0ac0f077c9de2f2f8
DysUji <ujamaashelton1994@gmail.com>
2024-10-11 08:43:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const expenseForm = document.getElementbyId('expense-form');
+const expenseList = document.getElementbyId('expense-list');
 
+expenseForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const expense = document.getbyElementbyId('expense').value;
+    const amount =  document.getbyElementbyId('amount').value;
+
+    addExpense(expense, amount);
+
+    expenseForm.reset();
+
+function addExpense(expense, amount) {
+  const row = document.createElement('tr');
+  const expenseCell = document.createElment('td')
+  const amountCell = document.createElement('td')
+
+  expenseCell.textContent = expense;
+  amountCell.textContent = amount;
+
+  rowappendChild(expenseCell);
+  rowappendChild(amountCell);
+
+  expenseList.appendChild(row);
